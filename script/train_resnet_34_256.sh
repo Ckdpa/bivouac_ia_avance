a82730404f8da4403e619d544ce5e759631ae44e
Ckdpa <felix.wirth@epita.com>
2023-01-29 23:18:09 +0100

    Adding training scripts for slurm

---
+++ script/train_resnet_34_256.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH -p short
+#SBATCH --time=06:00:00   # walltime
+#SBATCH --ntasks=16   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gres=gpu:4
+#SBATCH -J "test-resnet34-1024"   # job name
+#SBATCH --mail-user=felix.wirth@epita.fr   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+
+echo Job starting
+cd data
+python3.8 train_script_resnet34_256.py
